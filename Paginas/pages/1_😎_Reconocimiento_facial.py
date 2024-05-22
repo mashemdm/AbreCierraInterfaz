cd76feae7f3b5f870f245e9cd7e0d7da845c1da5
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-05-22 11:16:15 -0500

    Update 1_😎_Reconocimiento_facial.py

--- Paginas/pages/1_😎_Reconocimiento_facial.py
+++ Paginas/pages/1_😎_Reconocimiento_facial.py
@@ -55,6 +55,9 @@ if img_file_buffer is not None:
 
     # run the inference
     prediction = model.predict(data)
+    except Exception as e:
+    st.error(f"Error al predecir: {str(e)}")
+    raise
     print(prediction)
     if prediction[0][0]>0.3:
       st.header('Abriendo')
