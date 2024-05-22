bf43a50d644957c1eb9901fa77ad24276b69578a
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-05-22 13:56:06 -0500

    Update 1_😎_Reconocimiento_facial.py

--- pages/1_😎_Reconocimiento_facial.py
+++ pages/1_😎_Reconocimiento_facial.py
@@ -76,3 +76,44 @@ if img_file_buffer is not None:
       st.header('Te veo triste ☹️')
       client1.publish("CanalAbreCierra","{'gesto': 'Triste'}",qos=0, retain=False)
       time.sleep(0.2)  
+
+#Parte de hablar
+text = st.text_input("Ingrese el texto.")
+tld="es"
+
+def text_to_speech(text, tld):
+    
+    tts = gTTS(text,"es", tld, slow=False)
+    try:
+        my_file_name = text[0:20]
+    except:
+        my_file_name = "audio"
+    tts.save(f"temp/{my_file_name}.mp3")
+    return my_file_name, text
+    if st.button("convertir"):
+    result, output_text = text_to_speech(text, tld)
+    audio_file = open(f"temp/{result}.mp3", "rb")
+    audio_bytes = audio_file.read()
+    st.markdown(f"## Tú audio:")
+    st.audio(audio_bytes, format="audio/mp3", start_time=0)
+
+    #if display_output_text:
+    st.markdown(f"## Texto en audio:")
+    st.write(f" {output_text}")
+
+
+def remove_files(n):
+    mp3_files = glob.glob("temp/*mp3")
+    if len(mp3_files) != 0:
+        now = time.time()
+        n_days = n * 86400
+        for f in mp3_files:
+            if os.stat(f).st_mtime < now - n_days:
+                os.remove(f)
+                print("Deleted ", f)
+
+
+remove_files(7)
+
+
+
