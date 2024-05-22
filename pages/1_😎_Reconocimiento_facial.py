121c3e8913546dc2ca60f5c7f3705d49e56f9d6a
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-05-22 12:25:57 -0500

    Update 1_😎_Reconocimiento_facial.py

--- pages/1_😎_Reconocimiento_facial.py
+++ pages/1_😎_Reconocimiento_facial.py
@@ -70,9 +70,9 @@ if img_file_buffer is not None:
     print(prediction)
     if prediction[0][0]>0.3:
       st.header('Abriendo')
-      client1.publish("CanalAbreCierra","{'gesto': 'Abre'}",qos=0, retain=False)
+      client1.publish("CanalAbreCierra","{'gesto': 'Feliz'}",qos=0, retain=False)
       time.sleep(0.2)
     if prediction[0][1]>0.3:
       st.header('Cerrando')
-      client1.publish("CanalAbreCierra","{'gesto': 'Cierra'}",qos=0, retain=False)
+      client1.publish("CanalAbreCierra","{'gesto': 'Triste'}",qos=0, retain=False)
       time.sleep(0.2)  
