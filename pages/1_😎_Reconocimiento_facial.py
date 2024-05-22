05c7e4abcc53ec2f7da92cec10fd1c94fb60c339
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-05-22 14:17:21 -0500

    Update 1_😎_Reconocimiento_facial.py

--- pages/1_😎_Reconocimiento_facial.py
+++ pages/1_😎_Reconocimiento_facial.py
@@ -85,15 +85,6 @@ if img_file_buffer is not None:
     if prediction[0][1]>0.3:
       st.header('Te veo triste ☹')
         
-        result, output_text = text_to_speech(text, tld)
-        audio_file = open(f"temp/{result}.mp3", "rb")
-        audio_bytes = audio_file.read()
-        st.markdown(f"## Tú audio:")
-        st.audio(audio_bytes, format="audio/mp3", start_time=0)
-        st.markdown(f"## Texto en audio:")
-        st.write(f" {output_text}")
-
-        
       client1.publish("CanalAbreCierra","{'gesto': 'Triste'}",qos=0, retain=False)
       time.sleep(0.2)  
   
@@ -113,12 +104,16 @@ def text_to_speech(text, tld):
     return my_file_name, text
     
     
-"""if st.button("convertir"):
+if st.button("convertir"):
     result, output_text = text_to_speech(text, tld)
     audio_file = open(f"temp/{result}.mp3", "rb")
     audio_bytes = audio_file.read()
     st.markdown(f"## Tú audio:")
-    st.audio(audio_bytes, format="audio/mp3", start_time=0)"""
+    st.audio(audio_bytes, format="audio/mp3", start_time=0)
+
+ #if display_output_text:
+    st.markdown(f"## Texto en audio:")
+    st.write(f" {output_text}")
 
   
 
