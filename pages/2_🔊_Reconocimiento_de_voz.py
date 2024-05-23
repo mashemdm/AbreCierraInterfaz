a5b0b87289944ce0cac38dee88a1ae66e486eb13
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-05-22 20:25:16 -0500

    Update 2_🔊_Reconocimiento_de_voz.py

--- pages/2_🔊_Reconocimiento_de_voz.py
+++ pages/2_🔊_Reconocimiento_de_voz.py
@@ -3,6 +3,7 @@ import streamlit as st
 from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
+from PIL import Image
 import time
 import paho.mqtt.client as paho
 import json
