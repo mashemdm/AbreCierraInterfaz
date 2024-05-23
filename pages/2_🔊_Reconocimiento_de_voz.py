dec7f90b3be0093700ccbd601f5741de25414704
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-05-22 20:11:11 -0500

    Update 2_🔊_Reconocimiento_de_voz.py

--- pages/2_🔊_Reconocimiento_de_voz.py
+++ pages/2_🔊_Reconocimiento_de_voz.py
@@ -39,24 +39,7 @@ st.write("Toca el Botón y dime cómo te sientes")
 
 stt_button = Button(label="Dame click", width=200)
 
-stt_button.js_on_event("button_click", CustomJS(code="""
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
-
-    recognition.onresult = function (e) {
-        var value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                value += e.results[i][0].transcript;
-            }
-        }
-        if (value != "") {
-            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-        }
-    }
-    recognition.start();
-    """))
+stt_button.js_on_event("button_click")
 
 result = streamlit_bokeh_events(
     stt_button,
