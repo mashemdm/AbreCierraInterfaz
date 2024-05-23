4ab56dda907d71913d6c6a1a708b0c576c7db9db
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-05-22 20:04:30 -0500

    Update 2_🔊_Reconocimiento_de_voz.py

--- pages/2_🔊_Reconocimiento_de_voz.py
+++ pages/2_🔊_Reconocimiento_de_voz.py
@@ -3,50 +3,47 @@ import streamlit as st
 from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
-from PIL import Image
 import time
-import glob
 import paho.mqtt.client as paho
 import json
 from gtts import gTTS
 from googletrans import Translator
 
-#Mi codigo
+# Configuración de la página de Streamlit
 st.set_page_config(page_title="Reconocimiento de voz", page_icon="🔊")
-
 st.markdown("# Reconocimiento de voz")
 st.sidebar.header("Reconocimiento de voz")
 
-def on_publish(client,userdata,result):             #create function for callback
-    print("el dato ha sido publicado \n")
+# Función callback para la publicación de mensajes
+def on_publish(client, userdata, result):
+    print("El dato ha sido publicado\n")
     pass
 
+# Función callback para la recepción de mensajes
 def on_message(client, userdata, message):
     global message_received
     time.sleep(2)
-    message_received=str(message.payload.decode("utf-8"))
+    message_received = str(message.payload.decode("utf-8"))
     st.write(message_received)
 
-broker="157.230.214.127"
-port=1883
-client1= paho.Client("APPVOZ")
-client1.on_message = on_message
+# Configuración del broker MQTT
+broker = "broker.mqttdashboard.com"
+port = 1883
+client2 = paho.Client("APPVOZ")
+client2.on_message = on_message
+client2.on_publish = on_publish
+client2.connect(broker, port)
+client2.loop_start()
 
+st.write("Toca el Botón y dime cómo te sientes")
 
-#image = Image.open('voice_ctrl.jpg')
-
-#st.image(image, width=200)
-
-
-st.write("Toca el Botón y dime cómo te sientes ")
-
-stt_button = Button(label=" Dame click ", width=200)
+stt_button = Button(label="Dame click", width=200)
 
 stt_button.js_on_event("button_click", CustomJS(code="""
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
     recognition.interimResults = true;
- 
+
     recognition.onresult = function (e) {
         var value = "";
         for (var i = e.resultIndex; i < e.results.length; ++i) {
@@ -54,7 +51,7 @@ stt_button.js_on_event("button_click", CustomJS(code="""
                 value += e.results[i][0].transcript;
             }
         }
-        if ( value != "") {
+        if (value != "") {
             document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
         }
     }
@@ -67,19 +64,20 @@ result = streamlit_bokeh_events(
     key="listen",
     refresh_on_update=False,
     override_height=75,
-    debounce_time=0)
+    debounce_time=0
+)
 
 if result:
     if "GET_TEXT" in result:
         st.write(result.get("GET_TEXT"))
-        client1.on_publish = on_publish                            
-        client1.connect(broker,port)  
-        message =json.dumps({"gesto":result.get("GET_TEXT").strip()})
-       # ret= client1.publish("Voice666", message)
-        ret= client1.publish("CanalAbreCierra", message)
+        message = json.dumps({"gesto": result.get("GET_TEXT").strip()})
+        ret = client2.publish("CanalAbreCierra", message)
 
-    
     try:
         os.mkdir("temp")
     except:
         pass
+
+# Mantener la conexión MQTT activa
+client2.loop_stop()
+client2.disconnect()
