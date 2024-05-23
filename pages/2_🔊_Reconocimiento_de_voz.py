fb043301757813f9ca712612231f10d595d0931e
mashemdm <162599950+mashemdm@users.noreply.github.com>
2024-05-22 20:15:20 -0500

    Update 2_🔊_Reconocimiento_de_voz.py

--- pages/2_🔊_Reconocimiento_de_voz.py
+++ pages/2_🔊_Reconocimiento_de_voz.py
@@ -37,7 +37,8 @@ client2.loop_start()
 
 st.write("Toca el Botón y dime cómo te sientes")
 
-stt_button = Button(label="Dame click", width=200)
+#stt_button = Button(label="Dame click", width=200)
+stt_button = st.button(label="Dame click", width=200)
 
 stt_button.js_on_event("button_click", CustomJS(code="""
     var recognition = new webkitSpeechRecognition();
